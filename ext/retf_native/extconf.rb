4dadd471caef8edc3de0602d7dedd1f26912a0b6
The Major <spartan364@gmail.com>
2024-10-27 18:41:42 +0000

    minor CI and extconf update

--- ext/retf_native/extconf.rb
+++ ext/retf_native/extconf.rb
@@ -5,6 +5,7 @@ require 'mkmf'
 # required for converting network byte order
 # to host byte order and vice versa
 abort('endian.h is required') unless have_header('endian.h')
+abort('rb_str_strlen is required') unless have_func('rb_str_strlen')
 
 append_cflags('-flto')
 create_makefile('retf_native')
