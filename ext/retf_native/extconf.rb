03c8b43e03775f42d81a128dfecefce0b386ef41
The Major <spartan364@gmail.com>
2024-11-13 03:01:42 +0000

    fix double-quoting in extconf.rb

--- ext/retf_native/extconf.rb
+++ ext/retf_native/extconf.rb
@@ -6,13 +6,13 @@ require 'mkmf'
 # to host byte order and vice versa
 abort('endian.h is required') unless have_header('endian.h')
 
-have_func('rb_str_strlen', "ruby.h") # truffleruby
-have_func('rb_mod_name', "ruby.h") # truffleruby
-have_func('rb_big_eq', "ruby.h") # truffleruby
-have_func('rb_big_and', "ruby.h") # truffleruby
-have_func('rb_big_rshift', "ruby.h") # truffleruby
-have_func('rb_big_unpack', "ruby.h") # truffleruby
-have_func("rb_hash_bulk_insert", "ruby.h") # TruffleRuby
+have_func('rb_str_strlen', 'ruby.h') # truffleruby
+have_func('rb_mod_name', 'ruby.h') # truffleruby
+have_func('rb_big_eq', 'ruby.h') # truffleruby
+have_func('rb_big_and', 'ruby.h') # truffleruby
+have_func('rb_big_rshift', 'ruby.h') # truffleruby
+have_func('rb_big_unpack', 'ruby.h') # truffleruby
+have_func('rb_hash_bulk_insert', 'ruby.h') # TruffleRuby
 
 append_cflags('-flto')
 create_makefile('retf_native')
